448281fe62fa89f5ab29c748c407b7e1a15194d3
cmaulidy <c.maulidy@gmail.com>
2022-08-14 00:38:04 +0700

    Latihan Dasar

---
+++ 01 Dasar Java/LatihanOperatorAritmatika.java
@@ -0,0 +1,24 @@
+public class LatihanOperatorAritmatika {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c;
+        System.out.println("Operator Aritmatika :");
+        System.out.println("Penambahan : ");
+        c = a + b;
+        System.out.println("Isi c =" + c);
+        System.out.println("Pengurangan : ");
+        c = b - a;
+        System.out.println("Isi c =" + c);
+        System.out.println("Perkalian : ");
+        c = a * b;
+        System.out.println("Isi variabel c =" + c);
+        System.out.println("Pembagian : ");
+        c = b / a;
+        System.out.println("Isi variabel c =" + c);
+        System.out.println("Sisa Hasil Bagi : ");
+        c = b % a;
+        System.out.println("Isi variabel c =" + c);
+    }
+
+}
---
+++ 01 Dasar Java/LatihanVariable.java
@@ -0,0 +1,12 @@
+public class LatihanVariable {
+    public static void main(String[] args) {
+        char var1 = 'Z';
+        int var2 = 10;
+        double var3 = 0.8;
+        boolean var4 = true;
+        System.out.println("Isi variabel var1 : " + var1);
+        System.out.println("Isi variabel var2 : " + var2);
+        System.out.println("Isi variabel var3 : " + var3);
+        System.out.println("Isi variabel var4 : " + var4);
+    }
+}
