ac791a9d88ee5e387be2713167af3d64fd283f21
cmaulidy <c.maulidy@gmail.com>
2022-08-14 12:09:01 +0700

    Kondisi

---
+++ Pelatihan/Kondisi.java
@@ -0,0 +1,9 @@
+package Pelatihan;
+
+public class Kondisi {
+    public static void main(String[] args) {
+        int x = 11;
+        if (x == 10)
+            System.out.println("Nilai X = " + x);
+    }
+}
