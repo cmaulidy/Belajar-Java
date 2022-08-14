cd59421429431cd92f673a215e93bc73d6ab0692
cmaulidy <c.maulidy@gmail.com>
2022-08-14 12:32:27 +0700

    Kondisi With Input

--- Pelatihan/Kondisi.java
+++ Pelatihan/Kondisi.java
@@ -1,9 +1,21 @@
-package Pelatihan;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Kondisi {
-    public static void main(String[] args) {
-        int x = 11;
-        if (x == 10)
-            System.out.println("Nilai X = " + x);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int umur;
+        System.out.print("Masukkan Umur anda : "); // melakukan input nama
+        umur = Integer.parseInt(reader.readLine());
+
+        if (umur < 13) {
+            System.out.println("Anak anak  Usia = " + umur);
+        } else if (umur > 12 && umur < 21) {
+            System.out.println("Remaja  Usia = " + umur);
+        } else {
+            System.out.println("Dewasa = " + umur);
+        }
     }
 }
